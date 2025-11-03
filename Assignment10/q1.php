a40c809d4f916c3326cb0db748c25e2819730dca
Michael S <michael.smichael2023@vitstudent.ac.in>
2025-11-03 16:18:38 +0530

    Update q1.php

--- Assignment10/q1.php
+++ Assignment10/q1.php
@@ -1,1 +1,10 @@
 
+<?php
+if(isset($_COOKIE['visits'])) {
+    $count = $_COOKIE['visits'] + 1;
+} else {
+    $count = 1;
+}
+setcookie('visits', $count, time() + 3600);
+echo "Welcome! You have visited this page $count times.";
+?>
